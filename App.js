9dfd4510211fb3ba9a4d2c4b85c6a583761be492
FaziletOezisci <fazilet.oezisci@hotmail.de>
2018-05-20 12:25:48 +0200

    New feature FlatList

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
-  state = {item: null};
+  state = {items: []};
   render() {
+    let content = <Text>Keine Einträge im Tagebuch</Text>
+    if (this.state.items.length > 0) {
+      content = (
+        <FlatList
+        style = {styles.list}
+        data = {this.state.items}
+        renderItem = {({item}) => <Text>{item.text}</Text>}
+        keyExtractor = {item => item.date}
+        />
+      );
+    }
     return (
       <View style={styles.container}>
         <Text> {this.state.item || 'Keine Einträge im Tagebuch'}</Text>
@@ -13,7 +24,7 @@ export default class App extends React.Component {
         //onChangeText={(item) => this.setState({item})}/>
 
         //Use onSubmitEditing for refresh the text after pushing the Enter-Button 
-        onSubmitEditing = {event => this.setState({ item: event.nativeEvent.text})}/>
+        onSubmitEditing = {event => this.setState({ item: event.nativeEvent.text})}/>      
       </View>
     ); 
   }
@@ -24,7 +35,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
+  list: {
+    marginTop: 24
+  },
   input: {
     height: 40
-  },
+  }
 });
